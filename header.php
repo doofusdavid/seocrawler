15965d36dac24801259c417ac62858bd23fc1d87
David Edwards <david.edwards@gmail.com>
2018-03-11 19:31:52 -0600

    added SASS

--- header.php
+++ header.php
@@ -14,8 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="//code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="//code.jquery.com/ui/1.12.0/jquery-ui.min.js" integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="/css/screen.css">
-    <link href="//fonts.googleapis.com/css?family=Raleway:400,700,900" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 
 <body>
